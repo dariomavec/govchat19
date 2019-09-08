334d8fb0053b1a00e07cd3185de63d4cbcd9c581
Dario Mavec <dario.mavec@catena4.com>
2019-09-08 15:31:08 +1000

    zoom to tram

--- client/chat.js
+++ client/chat.js
@@ -24,7 +24,7 @@ function getClosestEntity(position) {
 		room = data.label;
 		setupWebSocket();
 
-        map.flyTo([ position.coords.latitude, position.coords.longitude ], 13);
+        map.flyTo([ data.lat, data.lon ], 13);
 	})
 
   //geotext.innerHTML = "Latitude: " + position.coords.latitude +
